8fdc0120c4a61e23c2a078d8174a434cacfb397b
H Hugo Falkman <hug@falkman.net>
2019-08-27 23:19:17 +0200

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by H Hugo Falkman on 2019-08-27.
+//  Copyright © 2019 H Hugo Falkman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
