339575ba9ae91c2e830919c64838c3ac5dfeca5b
hugofalkman <hugo@falkman.net>
2019-10-15 00:35:28 +0200

    Version in 100 days with SwiftUI complete

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -14,36 +14,60 @@ struct ContentView: View {
     
     @State private var score = 0
     @State private var alertTitle = ""
+    @State private var errorMessage = ""
     @State private var showingAlert = false
     
     var body: some View {
-        NavigationView {
-            VStack {
-                ForEach((0...2), id: \.self) { number in
-                    Image(self.countries[number])
-                        .border(Color.black, width: 1)
-                        .onTapGesture {
-                            self.flagTapped(number)
-                        }
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    
+                    Text(countries[correctAnswer].capitalized)
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
                 }
+                
+                ForEach((0...2), id: \.self) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                }
+                
+                Text("Your score is \(score)")
+                    .foregroundColor(.white)
+                    .font(.title)
+                
                 Spacer()
             }
-                .navigationBarTitle(Text(countries[correctAnswer].uppercased()))
-                .alert(isPresented: $showingAlert) {
-                    Alert(title: Text(alertTitle), message: Text("Your score is \(score)"), dismissButton: .default(Text("Continue")) {
-                            self.askQuestion()
-                        })
-                }
         }
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text(alertTitle), message: Text(errorMessage + "Your score is \(score)."), dismissButton: .default(Text("Continue")) {
+                        self.askQuestion()
+                    })
+            }
     }
     
     func flagTapped(_ tag: Int) {
         if tag == correctAnswer {
             score += 1
             alertTitle = "Correct"
+            errorMessage = ""
         } else {
             score -= 1
             alertTitle = "Wrong"
+            errorMessage = "That's the flag of " + countries[tag].capitalized + ". "
         }
         showingAlert = true
     }
